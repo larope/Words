ef43d9d1b5715a1fa421fbeba7b282f776ccc894
Artak <jarzop@gmail.com>
2023-11-08 16:05:08 +0400

    Changed colors, font added word completation with different colors

--- constants.js
+++ constants.js
@@ -1,11 +1,24 @@
-const BACKGROUD_COLOR = "#50386a";
-const LETTER_SIZE = 16;
+const BACKGROUD_COLOR = "#fefae0";
+const UNSELECTED_WORD_COLOR = "#353535";
+const SELECTED_WORD_COLOR = "#014f86";
+const SELECTED_WORD_COLOR_SUCCEED = "#8ac926";
+const SELECTED_WORD_COLOR_FAILED = "#ff595e";
 
-const WORDS = ["hen", "tumour", "morsel", "lend", "agent", "collar", "hook", "violation", "courage", "throne", "rape", "track", "handicap", "trouser", "heaven", "public", "asylum", "hospital", "broccoli", "ball", "morale", "wage", "reservoir", "composer", "owner", "lane", "shatter", "confuse", "dinner", "linear", "build", "national", "inspector", "shot", "hall", "horoscope", "belly", "joint", "bird", "wander", "slow", "ash", "squash", "tasty", "dance", "remark", "carry", "fate", "village", "fly"];
-const WORD_SPEED = 1;
+const TEXT_FONT = "ubuntu-regular";
+const SCREEN_WIDTH = window.innerWidth;
+const SCREEN_HEIGHT = window.innerHeight;
+const FONT_SIZE = SCREEN_HEIGHT*0.08;
 
+
+const WORDS = ["the", "of", "and", "a", "to", "in", "is", "you", "that", "it", "he", "was", "for", "on", "are", "with", "as", "I", "his", "they", "be", "at", "one", "have", "this", "from", "or", "had", "by", "hot", "word", "but", "what", "some", "we", "can", "out", "other", "were", "all", "there", "when", "up", "use", "your", "how", "said", "an", "each", "she", "which", "do", "their", "time", "if", "will", "way", "about", "many", "then", "them", "write", "would", "like", "so", "these", "her", "long", "make", "thing", "see", "him", "two", "has", "look", "more", "day", "could", "go", "come", "did", "number", "sound", "no", "most", "people", "my", "over", "know", "water", "than", "call", "first", "who", "may", "down", "side", "been", "now", "find", "any", "new", "work", "part", "take", "get", "place", "made", "live", "where", "after", "back", "little", "only", "round", "man", "year", "came", "show", "every", "good", "me", "give", "our", "under", "name", "very", "through", "just", "form", "sentence", "great", "think", "say", "help", "low", "line", "before", "turn", "cause", "same", "mean", "differ", "move", "right", "boy", "old", "too", "does", "tell", "sentence", "set", "three", "want", "air", "well", "also", "play", "small", "end", "put", "home", "read", "hand", "port", "large", "spell", "add", "even", "land", "here", "must", "big", "high", "such", "follow", "act", "why", "men", "change", "went", "light", "kind", "off", "need", "house", "picture", "try", "us", "again", "animal", "point", "mother", "world", "near", "build", "self", "earth", "father", "head", "stand", "own", "page", "should", "country", "found", "answer", "school", "grow", "study", "still", "learn", "plant", "cover", "food", "sun", "four", "thought", "let", "keep", "eye", "never", "last", "door", "between", "city", "tree", "cross", "since", "hard", "start", "might", "story", "saw", "far", "sea", "draw", "left", "late", "run", "don't", "while", "press", "close", "night", "real", "life", "few", "stop", "open", "seem", "together", "next", "white", "children", "begin", "got", "walk", "example", "ease", "paper", "group", "always", "music", "those", "both", "mark", "often", "letter", "until", "mile", "river", "car", "feet", "care", "second", "book", "carry", "took", "science", "eat", "room", "friend", "began", "idea", "fish", "mountain", "stop", "once", "base", "hear", "horse", "cut", "sure", "watch", "color", "face", "wood", "main", "enough", "plain", "girl", "usual", "young", "ready", "above", "ever", "red", "list", "though", "feel", "talk", "bird", "soon", "body", "dog", "family", "direct", "pose", "leave", "song", "measure", "state", "product", "black", "short", "numeral", "class", "wind", "question", "happen", "complete", "ship", "area", "half", "rock", "order", "fire", "south", "problem", "piece", "told", "knew", "pass", "since", "top", "whole", "king", "space", "heard", "best", "hour", "better", "true", "during", "hundred", "five", "remember", "step", "early", "hold", "west", "ground", "interest", "reach", "fast", "verb", "sing", "listen", "six", "table", "travel", "less", "morning", "ten", "simple", "several", "vowel", "toward", "war", "lay", "against", "pattern", "slow", "center", "love", "person", "money", "serve", "appear", "road", "map", "rain", "rule", "govern", "pull", "cold", "notice", "voice", "unit", "power", "town", "fine", "certain", "fly", "fall", "lead", "cry", "dark", "machine", "note", "wait", "plan", "figure", "star", "box", "noun", "field", "rest", "correct", "able", "pound", "done", "beauty", "drive", "stood", "contain", "front", "teach", "week", "final", "gave", "green", "oh", "quick", "develop", "ocean", "warm", "free", "minute", "strong", "special", "mind", "behind", "clear", "tail", "produce", "fact", "street", "inch", "multiply", "nothing", "course", "stay", "wheel", "full",
+    "force", "blue", "object", "decide",];
+//const WORDS = ["the", "of", "and", "a", "to", "in"]
+const WORD_SPEED = SCREEN_HEIGHT/20;
+const MIN_LETTER_COUNT = 4;
 const MAX_LETTER_COUNT = 10;
-const MAX_WORD_SIZE = LETTER_SIZE * MAX_LETTER_COUNT;
-const MAX_WORD_X = window.innerWidth - MAX_WORD_SIZE;
-const INITIAL_SPAWNING_INTERVAL = 3000; // 3s
-const SPAWNING_INTERVAL_ACCELERATION = 1000; // 0.1s
+const MAX_WORD_SIZE = FONT_SIZE * MAX_LETTER_COUNT;
+const INITIAL_SPAWNING_INTERVAL = 1500; // in miliseconds
+const SPAWNING_INTERVAL_MIN = 1300;
+const SPAWNING_INTERVAL_ACCELERATION = -90; // in miliseconds
+
+const MUSIC_WORD_COMPLETE = "/Users/artakbaghdasaryan/fleet/Words/gfx/WordCompleteSound.wav";
--- script.js
+++ script.js
@@ -1,26 +1,94 @@
 const words = getWords();
-const visibleWords = [];
+let visibleWords = [];
+let lastEnteredLetter = "";
+let selectedWord = false;
+let currentCharColor = SELECTED_WORD_COLOR;
+let spawningInterval = INITIAL_SPAWNING_INTERVAL;
+let scoreText = {
+    x:10,
+    y:70,
+    score: 0
+}
 
 function setup() {
-    createCanvas(window.innerWidth, window.innerHeight);
+    createCanvas(SCREEN_WIDTH, SCREEN_HEIGHT);
+    textSize(FONT_SIZE);
     words.forEach((item) => {
-        item.x = random(MAX_WORD_X);
+        item.x = random(0, SCREEN_WIDTH-textWidth(item.text));
         item.y = 0;
     })
-    play();
+    textFont(TEXT_FONT); // Use the font-family defined in the CSSplaySound
+    setTimeout(AddWord(), spawningInterval);
+
 }
 
 function draw() {
     background(BACKGROUD_COLOR);
 
+    let deltaTime = 1/frameRate();
+
     for(let word of visibleWords) {
-        text(word.text, word.x, Math.floor(word.y));
-        word.y += WORD_SPEED;
+        if(word == undefined) continue;
+        let textToDraw;
+        if(word.text == selectedWord.text){
+            textToDraw = [{text : selectedWord.text.slice(0, selectedWord.index), color : SELECTED_WORD_COLOR_SUCCEED},
+                {text : selectedWord.text[selectedWord.index], color: currentCharColor},
+                {text : selectedWord.text.slice(selectedWord.index+1, selectedWord.text.length), color : SELECTED_WORD_COLOR}]
+        }else{
+            textToDraw = [{text : word.text,color : UNSELECTED_WORD_COLOR}]
+        }
+
+        let x = word.x;
+        for(let wordToDraw of textToDraw){
+            textSize(FONT_SIZE);
+            fill(wordToDraw.color);
+            text(wordToDraw.text, x, Math.floor(word.y));
+            x+=textWidth(wordToDraw.text);
+        }
+        word.y += WORD_SPEED*deltaTime;
     }
+    if(visibleWords[0] != undefined && visibleWords[0].y > SCREEN_HEIGHT) lose();
+    fill("#023e8a");
+    text("Your score is " + scoreText.score, scoreText.x, scoreText.y);
 }
 
+
+function win(intervalId) {
+    clearInterval(intervalId);
+    noLoop();
+    alert("You won");
+}
+//str.slice(0, index)
+function selectWord(firstLetter){
+    for(word of visibleWords) if(word.text[0] == firstLetter) return word;
+    return false;
+}
+
+function keyTyped() {
+    if(selectedWord == false) selectedWord = selectWord(key);
+    fillSelectedWord(key);
+}
+function fillSelectedWord(letter){
+    if(selectedWord == false) return;
+
+    let expectedLetter = selectedWord.text[selectedWord.index]
+    currentCharColor = expectedLetter == letter ? SELECTED_WORD_COLOR : SELECTED_WORD_COLOR_FAILED;
+
+    if(expectedLetter == letter) selectedWord.index++;
+    if(selectedWord.index == selectedWord.text.length) completeWord();
+}
+function completeWord(){
+    scoreText.score++;
+    playSound(MUSIC_WORD_COMPLETE);
+    let word = 0;
+    for(word in visibleWords) if(visibleWords[word].text == selectedWord.text) break;
+
+    visibleWords.splice(word, 1);
+    selectedWord = false;
+}
 function getWords() {
-    const words = WORDS.filter(word => word.length <= MAX_LETTER_COUNT)
+    const words = WORDS.filter(word => word.length <= MAX_LETTER_COUNT && word.length >= MIN_LETTER_COUNT)
+    shuffle(words);
 
     return words.map((word) => ({
         text: word,
@@ -30,20 +98,44 @@ function getWords() {
     }));
 }
 
-function play() {
-    let spawningInterval = INITIAL_SPAWNING_INTERVAL;
-    const intervalId = setInterval(() => {
-        visibleWords.push(words.pop());
-        spawningInterval -= SPAWNING_INTERVAL_ACCELERATION;
-
-        if(spawningInterval <= 0) {
-            win(intervalId);
-        }
-    }, spawningInterval);
+let AddWord = () => {
+    visibleWords.push(words.pop());
+    spawningInterval = Math.max(spawningInterval+SPAWNING_INTERVAL_ACCELERATION, SPAWNING_INTERVAL_MIN)
+    if(words.length <= 0 && visibleWords.length <= 0) return win();
+    setTimeout(AddWord, spawningInterval);
 }
 
-function win(intervalId) {
-    clearInterval(intervalId);
+const playSound = (path) => {
+    let audio = new Audio(path);
+
+    audio.mozPreservesPitch = false;
+    audio.playbackRate =(Math.random()+1);
+    audio.play();
+}
+
+
+
+function win(){
+    alert("You won!");
+}
+function lose(){
     noLoop();
-    alert("You won");
+    alert("You lose!")
+}
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
 }
